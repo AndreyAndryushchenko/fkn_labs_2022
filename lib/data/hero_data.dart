9f32f2cf0a7247dc2ea196b4746442035a57b0a6
Anton <ant9901on@mail.ru>
2022-11-03 23:23:31 +0600

    add hero description in class HeroData

--- lib/data/hero_data.dart
+++ lib/data/hero_data.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
-import '../widgets/hero_card.dart';
-
 class HeroData {
   final String heroName;
   final String pathToImage;
+  final String heroDescription;
 
-  const HeroData({required this.heroName, required this.pathToImage});
+  const HeroData(
+      {required this.heroName,
+      required this.pathToImage,
+      required this.heroDescription});
 }
 
 const Map<int, Color> heroBackgroundColor = {
@@ -20,11 +22,30 @@ const Map<int, Color> heroBackgroundColor = {
 };
 
 const List<HeroData> heroesData = [
-  HeroData(heroName: 'Deadpool', pathToImage: 'assets/deadpool.jpg'),
-  HeroData(heroName: 'Iron Man', pathToImage: 'assets/iron_man.jpg'),
-  HeroData(heroName: 'Captain America', pathToImage: 'assets/captain_america.jpg'),
-  HeroData(heroName: 'Spider Man', pathToImage: 'assets/spider_man.jpg'),
-  HeroData(heroName: 'Doctor Strange', pathToImage: 'assets/doctor_strange.jpg'),
-  HeroData(heroName: 'Thor', pathToImage: 'assets/thor.jpg'),
-  HeroData(heroName: 'Thanos', pathToImage: 'assets/thanos.jpg'),
+  HeroData(
+      heroName: 'Deadpool',
+      pathToImage: 'assets/deadpool.jpg',
+      heroDescription: ''),
+  HeroData(
+      heroName: 'Iron Man',
+      pathToImage: 'assets/iron_man.jpg',
+      heroDescription: ''),
+  HeroData(
+      heroName: 'Captain America',
+      pathToImage: 'assets/captain_america.jpg',
+      heroDescription: ''),
+  HeroData(
+      heroName: 'Spider Man',
+      pathToImage: 'assets/spider_man.jpg',
+      heroDescription: ''),
+  HeroData(
+      heroName: 'Doctor Strange',
+      pathToImage: 'assets/doctor_strange.jpg',
+      heroDescription: ''),
+  HeroData(
+      heroName: 'Thor', pathToImage: 'assets/thor.jpg', heroDescription: ''),
+  HeroData(
+      heroName: 'Thanos',
+      pathToImage: 'assets/thanos.jpg',
+      heroDescription: ''),
 ];
